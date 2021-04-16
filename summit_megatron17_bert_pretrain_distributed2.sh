d8d3a8ce69a05a9062d1d564ed1ae10bfe4d2fb9
Wei Wu <wwu12@login2.summit.olcf.ornl.gov>
2021-04-16 04:25:37 -0400

    update summit

--- summit_megatron17_bert_pretrain_distributed2.sh
+++ summit_megatron17_bert_pretrain_distributed2.sh
@@ -8,8 +8,8 @@ GPUS_PER_NODE=6
 # Change for multinode config
 MASTER_ADDR=$head
 MASTER_PORT=29501
-NNODES=$summit_nnodes
-#NNODES=4
+#NNODES=$summit_nnodes
+NNODES=4
 NODE_RANK=$OMPI_COMM_WORLD_RANK
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
@@ -17,10 +17,10 @@ DATA_PATH=my-bert_text_sentence
 CHECKPOINT_PATH=checkpoints/bert_345m
 
 export RANK=$NODE_RANK
-export LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
-export WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
-export MASTER_ADDR=$head
-export MASTER_PORT=29501
+#export LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
+export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE
+#export MASTER_ADDR=$head
+#export MASTER_PORT=29501
 echo "nnodes=${NNODES}"
 echo "Setting env_var RANK=${RANK}"
 echo "Setting env_var LOCAL_RANK=${LOCAL_RANK}"
@@ -28,23 +28,20 @@ echo "Setting env_var WORLD_SIZE=${OMPI_COMM_WORLD_SIZE}"
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
-#python -m torch.distributed.launch $DISTRIBUTED_ARGS \
-python megatron-lm-1.7/pretrain_bert.py \
-       --local_rank ${LOCAL_RANK} \
-       --tensor-model-parallel-size 2 \
-       --pipeline-model-parallel-size 2 \
+python -m torch.distributed.launch $DISTRIBUTED_ARGS \
+       megatron-lm-1.7/pretrain_bert.py \
+       --tensor-model-parallel-size 1 \
        --num-layers 24 \
        --hidden-size 1024 \
        --num-attention-heads 16 \
-       --batch-size 2 \
-       --num-microbatches-in-minibatch 2 \
+       --batch-size 4 \
        --seq-length 512 \
        --max-position-embeddings 512 \
        --train-iters 1000000 \
        --save $CHECKPOINT_PATH \
        --load $CHECKPOINT_PATH \
        --data-path $DATA_PATH \
-       --vocab-file bert-vocab.txt \
+       --vocab-file bert-large-uncased-vocab.txt \
        --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend nccl \
