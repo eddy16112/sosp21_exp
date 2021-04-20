393629e2e5457eb48368355e259fc79cbff4a483
Wei Wu <eddy16112@gmail.com>
2021-04-19 19:50:09 -0600

    more work on deepspeed

--- deepspeed_115zero_bert_pretrain_mp.sh
+++ deepspeed_115zero_bert_pretrain_mp.sh
@@ -3,10 +3,13 @@
 GPUS_PER_NODE=1
 # Change for multinode config
 MASTER_ADDR=cn135
-MASTER_PORT=6003
+MASTER_PORT=6001
 NNODES=2
 NODE_RANK=$OMPI_COMM_WORLD_RANK
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+DATA_PATH=my-bert_text_sentence
+CHECKPOINT_PATH=checkpoints/bert_345m
 
 export RANK=$NODE_RANK
 export LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
@@ -23,7 +26,7 @@ echo "Setting env_var LOCAL_SIZE=${LOCAL_SIZE}"
 
 #DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
-python DeepSpeedExamples/Megatron-LM/pretrain_bert.py \
+python DeepSpeedExamples/Megatron-LM-v1.1.5-ZeRO3/pretrain_bert.py \
        --local_rank ${LOCAL_RANK} \
        --model-parallel-size 1 \
        --num-layers 24 \
@@ -31,22 +34,22 @@ python DeepSpeedExamples/Megatron-LM/pretrain_bert.py \
        --num-attention-heads 16 \
        --batch-size 4 \
        --seq-length 512 \
-       --max-preds-per-seq 80 \
        --max-position-embeddings 512 \
        --train-iters 30 \
-			 --log-interval 10 \
-       --resume-dataloader \
-       --train-data wikipedia \
-       --tokenizer-type BertWordPieceTokenizer \
-       --tokenizer-model-type bert-large-uncased \
-       --presplit-sentences \
-       --cache-dir cache \
+       --data-path $DATA_PATH \
+       --vocab-file bert-large-uncased-vocab.txt \
+       --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend nccl \
        --lr 0.0001 \
        --lr-decay-style linear \
+       --min-lr 1.0e-5 \
        --lr-decay-iters 990000 \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
-       --warmup .01
+       --warmup .01 \
+       --log-interval 10 \
+       --save-interval 10000 \
+       --eval-interval 1000 \
+       --eval-iters 10
 
