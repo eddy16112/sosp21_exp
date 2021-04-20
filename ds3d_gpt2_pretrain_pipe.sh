36f0fd92a3cf5b5b07f14f21bb766dfeac7a6f65
Wei Wu <eddy16112@gmail.com>
2021-04-20 16:10:04 -0600

    ds 3d gpt2

---
+++ ds3d_gpt2_pretrain_pipe.sh
@@ -0,0 +1,152 @@
+#! /bin/bash
+
+GPUS_PER_NODE=4
+# Change for multinode config
+MASTER_ADDR=cn4030
+MASTER_PORT=6001
+NNODES=1
+NODE_RANK=$OMPI_COMM_WORLD_RANK
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+export DLWS_NUM_WORKER=${NNODES}
+export DLWS_NUM_GPU_PER_WORKER=${GPUS_PER_NODE}
+
+export RANK=$NODE_RANK
+export LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
+export WORLD_SIZE=$WORLD_SIZE
+export MASTER_ADDR=$MASTER_ADDR
+export MASTER_PORT=$MASTER_PORT
+
+echo "nnodes=${NNODES}"
+echo "master=${MASTER_ADDR}"
+echo "Setting env_var RANK=${RANK}"
+echo "Setting env_var LOCAL_RANK=${LOCAL_RANK}"
+echo "Setting env_var WORLD_SIZE=${OMPI_COMM_WORLD_SIZE}"
+
+DATA_PATH=my-gpt2_text_document
+VOCAB_PATH=gpt2-vocab.json
+MERGE_PATH=gpt2-merges.txt
+CHECKPOINT_PATH=checkpoints/gpt2_345m_ds
+
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+#config_json="$script_dir/ds_zero_stage_2_config.json"
+config_json="$script_dir/ds3d_gpt2_config.json"
+
+# Megatron Model Parallelism
+mp_size=2
+# DeepSpeed Pipeline parallelism
+pp_size=2
+
+NLAYERS=24
+NHIDDEN=1024
+BATCHSIZE=8
+LOGDIR="tensorboard_data/${NLAYERS}l_${NHIDDEN}h_${NNODES}n_${GPUS_PER_NODE}g_${pp_size}pp_${mp_size}mp_${BATCHSIZE}b_ds4"
+
+GAS=1
+
+#ZeRO Configs
+stage=0
+reduce_scatter=true
+contigious_gradients=true
+rbs=50000000
+agbs=5000000000
+
+#Actication Checkpointing and Contigious Memory
+chkp_layers=1
+PA=true
+PA_CPU=false
+CC=true
+SYNCHRONIZE=true
+PROFILE=false
+
+
+gpt_options=" \
+        --model-parallel-size ${mp_size} \
+        --pipe-parallel-size ${pp_size} \
+        --num-layers $NLAYERS \
+        --hidden-size $NHIDDEN \
+        --num-attention-heads 16 \
+        --seq-length 512 \
+        --max-position-embeddings 512 \
+        --batch-size $BATCHSIZE \
+        --gas $GAS \
+        --train-iters 30 \
+        --lr-decay-iters 320000 \
+        --data-path $DATA_PATH \
+        --vocab-file $VOCAB_PATH \
+        --merge-file $MERGE_PATH \
+        --data-impl mmap \
+        --split 949,50,1 \
+        --distributed-backend nccl \
+        --lr 1.5e-4 \
+        --lr-decay-style cosine \
+        --min-lr 1.0e-5 \
+        --weight-decay 1e-2 \
+        --clip-grad 1.0 \
+        --warmup 0.01 \
+        --checkpoint-activations \
+        --log-interval 10 \
+        --save-interval 500 \
+        --eval-interval 100 \
+        --eval-iters 100 \
+        --tensorboard-dir ${LOGDIR}
+"
+  
+ deepspeed_options=" \
+	              --deepspeed_mpi \
+                --deepspeed \
+                --deepspeed_config ${config_json} \
+                --zero-stage ${stage} \
+                --zero-reduce-bucket-size ${rbs} \
+                --zero-allgather-bucket-size ${agbs} 
+            "
+
+if [ "${contigious_gradients}" = "true" ]; then
+deepspeed_options="${deepspeed_options} \
+                --zero-contigious-gradients"
+fi
+
+if [ "${reduce_scatter}" = "true" ]; then
+deepspeed_options="${deepspeed_options} \
+                --zero-reduce-scatter"
+fi
+
+chkp_opt=" \
+--checkpoint-activations \
+--checkpoint-num-layers ${chkp_layers}"
+
+if [ "${PA}" = "true" ]; then
+chkp_opt="${chkp_opt} \
+        --partition-activations"
+fi
+
+if [ "${PA_CPU}" = "true" ]; then
+chkp_opt="${chkp_opt} \
+        --checkpoint-in-cpu"
+fi
+
+if [ "${SYNCHRONIZE}" = "true" ]; then
+chkp_opt="${chkp_opt} \
+        --synchronize-each-layer"
+fi
+
+if [ "${CC}" = "true" ]; then
+chkp_opt="${chkp_opt} \
+        --contigious-checkpointing"
+fi
+
+if [ "${PROFILE}" = "true" ]; then
+chkp_opt="${chkp_opt} \
+        --profile-backward"
+fi
+
+full_options="${gpt_options} ${deepspeed_options} ${chkp_opt}"
+
+#run_cmd="deepspeed --hostfile=hostfile --num_nodes ${DLWS_NUM_WORKER} --num_gpus ${DLWS_NUM_GPU_PER_WORKER} DeepSpeedExamples/Megatron-LM-v1.1.5-3D_parallelism/pretrain_gpt2.py $@ ${full_options}"
+run_cmd="python DeepSpeedExamples/Megatron-LM-v1.1.5-3D_parallelism/pretrain_gpt2.py $@ ${full_options}"
+echo ${run_cmd}
+
+python DeepSpeedExamples/Megatron-LM-v1.1.5-3D_parallelism/pretrain_gpt2.py $@ ${full_options}
+
+set +x
